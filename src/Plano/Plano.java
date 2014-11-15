c02c2dfef5fd53d3f28cebf4efbc9b8f413b6678
joaquinbf@live.com <joaquinbf@live.com@ca99f4f7-bc75-fa1d-a80f-d705e4e3179e>
2014-11-15 17:57:55 +0000

    Armando clase Plano

--- src/Plano/Plano.java
+++ src/Plano/Plano.java
@@ -1,12 +1,16 @@
-package Plano;
-
-import Conectores.Conexion;
-
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Plano {
-	Plano(){}
-	public void agregarConexion(Conexion unaConexion){
-		
+
+	private HashMap<String, Hectarea> hectareas;
+	private HashMap<Hectarea, ArrayList<Hectarea>> conexiones;
+ 
+	public Plano(int dimensionN, int dimensionM) {
+		hectareas = new HashMap<String, Hectarea>();
+		conexiones = new HashMap<Hectarea, ArrayList<Hectarea>>();
 	}
+
+
 }
