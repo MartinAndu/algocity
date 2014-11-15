3b89065bf34d303eb6572190bcc39edb85f82e3f
joaquinbf@live.com <joaquinbf@live.com@ca99f4f7-bc75-fa1d-a80f-d705e4e3179e>
2014-11-15 00:36:42 +0000

    pequeños arreglos.. buena idea usar constantes... ojo; usarlas bien

--- trunk/src/algo3/tp2/EdificioIndustrial.java
+++ trunk/src/algo3/tp2/EdificioIndustrial.java
@@ -2,14 +2,12 @@ package algo3.tp2;
 
 public class EdificioIndustrial extends Edificio {
 	
-	static int COSTO = 10;
+	static int COSTO_CONSTRUCCION = 10;
 	static int CONSUMO_ELECTRICO = 5;
 	static int CAPACIDAD_MAXIMA_EMPLEADOS = 25;
 	protected int cantidadEmpleados;
 	
 	public EdificioIndustrial(){
-		costoDeConstruccion = COSTO;
-		consumoElectrico = CONSUMO_ELECTRICO;
 		tieneAgua = false;
 		tieneElectricidad = false;
 		tieneAccesoAlTransito = false;
