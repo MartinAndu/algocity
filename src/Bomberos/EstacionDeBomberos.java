a99cd148e2f3b5d159145e525d0490279ea6b5cc
naku07@gmail.com <naku07@gmail.com@ca99f4f7-bc75-fa1d-a80f-d705e4e3179e>
2014-11-16 02:45:18 +0000

    Commiteo para probar (Nahuel)

---
+++ src/Bomberos/EstacionDeBomberos.java
@@ -0,0 +1,28 @@
+package Bomberos;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import Edificios.Reconstruible;
+
+public class EstacionDeBomberos {
+	ArrayList<Reconstruible> edificiosAReconstruir;
+	protected int costoDeConstruccion;
+	
+	public EstacionDeBomberos(){
+		edificiosAReconstruir = new ArrayList<Reconstruible>();
+	}
+	
+	public void agregarReconstruible(Reconstruible unReconstruible) {
+		edificiosAReconstruir.add(unReconstruible);
+	}
+
+	public void realizarReparaciones() {
+		Iterator<Reconstruible> it = edificiosAReconstruir.iterator();
+		while(it.hasNext()){
+			Reconstruible edificioAReconstruir = (Reconstruible)it.next();
+			edificioAReconstruir.reconstruir();
+			//Esto es lo que voy a cambiar para pasarle el parametro de puntos a reconstruir.
+		}
+	}
+}
