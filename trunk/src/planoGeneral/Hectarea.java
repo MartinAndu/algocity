c24a09e8fd17f51432b3cf8375a1afb5d08df8a4
joaquinbf@live.com <joaquinbf@live.com@ca99f4f7-bc75-fa1d-a80f-d705e4e3179e>
2014-11-17 06:10:25 +0000

    Plano ahora puede recorrese entorno a una posicion...Las pruebas estan realizadas y dan OK

--- trunk/src/planoGeneral/Hectarea.java
+++ trunk/src/planoGeneral/Hectarea.java
@@ -1,5 +1,20 @@
 package planoGeneral;
 
 public class Hectarea {
+	
+	public String identi;
+
+	public Hectarea() {
+		
+		this.identi = java.util.UUID.randomUUID().toString(); 
+	}
+	
+	@Override
+	public boolean equals (Object o) {
+	    Hectarea x = (Hectarea) o;
+	        if (x.identi == identi) return true;
+	        return false;
+	}
+
 
 }
--- trunk/src/planoGeneral/Plano.java
+++ trunk/src/planoGeneral/Plano.java
@@ -1,6 +1,7 @@
 package planoGeneral;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 import Edificios.Posicion;
 
@@ -11,7 +12,6 @@ public class Plano {
 	private HashMap<Hectarea, ArrayList<Hectarea>> caminos;
  
 	public Plano(int dimensionN, int dimensionM) {
-		
 		hectareas = new HashMap<String, Hectarea>();
 		caminos = new HashMap<Hectarea, ArrayList<Hectarea>>();
 		
@@ -19,16 +19,15 @@ public class Plano {
 	}
 
 	private void construirPlano(int dimensionN, int dimensionM) {
-		
 		construirHectareas(dimensionN, dimensionM);
 		construirCaminos(dimensionN, dimensionM);	
 	}
 
 	private void construirCaminos(int dimensionN, int dimensionM) {
 		
-		for (int i = 0; i < dimensionN; i++) {
-			for (int j = 0; j < dimensionM; j++) {
-				
+		for (int i = 1; i <= dimensionN; i++) {
+			
+			for (int j = 1; j <= dimensionM; j++) {	
 				Posicion posicion = new Posicion(i, j);				
 				ArrayList<Hectarea> hectareasAlrededor = obtenerHectareasDelEntorno(posicion);
 				Hectarea hectarea = hectareas.get(posicion.enString());
@@ -74,17 +73,19 @@ public class Plano {
 	private void agregarHectareaAlEntorno(
 			ArrayList<Hectarea> hectareasDelEntorno, String posicion) {
 		
-		if (hectareas.containsKey(posicion)) {
-				
-				Hectarea hectarea = hectareas.get(posicion);
-				hectareasDelEntorno.add(hectarea);
-			}
+		if (hectareas.containsKey(posicion)) {	
+			Hectarea hectarea = hectareas.get(posicion);
+			hectareasDelEntorno.add(hectarea);
+		}
+		else {
+			hectareasDelEntorno.add(null);
+		}
 	}
 
 	private void construirHectareas(int dimensionN, int dimensionM) {
 		
-		for (int i = 0; i < dimensionN; i++) {
-			for (int j = 0; j < dimensionM; j++) {
+		for (int i = 1; i <= dimensionN; i++) {
+			for (int j = 1; j <= dimensionM; j++) {
 				
 				String posicionEnString = (new Posicion(i, j)).enString();
 				Hectarea hectarea = new Hectarea();
@@ -93,5 +94,59 @@ public class Plano {
 		}
 	}
 
+	public Recorrido recorrerEntorno(Posicion posicion, int radioHectarea) {
+		
+		Hectarea hectarea = hectareas.get(posicion.enString());
+		ArrayList<Hectarea> entornoARecorrer = new ArrayList<Hectarea>();
+		ArrayList<Hectarea> entornoDelEntorno = new ArrayList<Hectarea>();
+		entornoDelEntorno.add(hectarea);
+		entornoARecorrer.add(hectarea);
+		
+		for (int i = 0; i < radioHectarea; i++) {
+			entornoDelEntorno = obtenerEntornoGeneral(entornoDelEntorno, entornoARecorrer);
+			entornoARecorrer.addAll(entornoDelEntorno);
+		}
+		
+		entornoARecorrer.remove(hectarea);
+		Recorrido recorrido = new Recorrido(entornoARecorrer);
+ 		
+		return recorrido;
+	}
 
+	private ArrayList<Hectarea> obtenerEntornoGeneral(
+			ArrayList<Hectarea> entornoDelEntorno,
+			ArrayList<Hectarea> entornoARecorrer) {
+		
+		ArrayList<Hectarea> entornoBuscado = new ArrayList<Hectarea>();
+		Iterator<Hectarea> it = entornoDelEntorno.iterator();
+		
+		while (it.hasNext()) {
+			Hectarea hectarea = (Hectarea) it.next();
+			ArrayList<Hectarea> entornoParcial = new ArrayList<Hectarea>();
+			
+			if (hectarea != null) {
+				entornoParcial = obtenerEntornoParcial(hectarea, entornoARecorrer, entornoBuscado);
+			}
+			entornoBuscado.addAll(entornoParcial);
+		}
+		return entornoBuscado;
+	}
+
+	private ArrayList<Hectarea> obtenerEntornoParcial(Hectarea hectarea,
+			ArrayList<Hectarea> entornoARecorrer, ArrayList<Hectarea> entornoBuscado) {
+		
+		ArrayList<Hectarea> entorno = caminos.get(hectarea);
+		ArrayList<Hectarea> entornoParcial = new ArrayList<Hectarea>();
+		Iterator<Hectarea> it = entorno.iterator();
+		
+		while (it.hasNext()) {
+			Hectarea hectareaEnEntorno = (Hectarea) it.next();
+			boolean hectareaNoEsta = !(entornoARecorrer.contains(hectareaEnEntorno)) && !(entornoBuscado.contains(hectareaEnEntorno));
+			if ((hectareaEnEntorno != null) && hectareaNoEsta){
+				entornoParcial.add(hectareaEnEntorno);
+			}
+		}
+		
+		return entornoParcial;
+	}
 }
