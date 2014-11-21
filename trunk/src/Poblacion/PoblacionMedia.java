5a5b230a3174dacf23271075b359b4d34f40d552
joaquinbf@live.com <joaquinbf@live.com@ca99f4f7-bc75-fa1d-a80f-d705e4e3179e>
2014-11-21 21:41:02 +0000

    Haciendo la clase jugador... aun esta en fase de prueba despues informo de algunos problemas... pensando en el controlador

---
+++ trunk/src/Poblacion/PoblacionMedia.java
@@ -0,0 +1,10 @@
+package Poblacion;
+
+
+public class PoblacionMedia extends Poblacion {
+
+	public PoblacionMedia() {
+		this.cantHabitantesCiudad = 500;
+		this.capacidadHabitacionalCiudad = 0;
+	}
+}
