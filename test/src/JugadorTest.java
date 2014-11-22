a088409c0bc8d14eb4185cf37ea1c51d18e1224a
joaquinbf@live.com <joaquinbf@live.com@ca99f4f7-bc75-fa1d-a80f-d705e4e3179e>
2014-11-22 16:33:35 +0000

    Arregle Generador de superficies para que anden mis pruebas pero otras fallaron... se debe a que al generar alternadamente .... cuando se crea un plano la primera fila respeta ese alternado ... las siguientes filas... se vuelve un poco impredecible

--- test/src/JugadorTest.java
+++ test/src/JugadorTest.java
@@ -72,7 +72,7 @@ public class JugadorTest {
 		
 		assertEquals(5000, jugador.obtenerPresupuesto());
 		
-		Posicion posicion = new Posicion(1, 1);
+		Posicion posicion = new Posicion(2, 2);
 		
 		jugador.crearPozoDeAgua(posicion);
 		
