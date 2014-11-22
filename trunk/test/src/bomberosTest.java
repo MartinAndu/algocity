0e55e7957d1b98a88ddce49ca224584dfdae1865
naku07@gmail.com <naku07@gmail.com@ca99f4f7-bc75-fa1d-a80f-d705e4e3179e>
2014-11-22 22:51:52 +0000

    Arreglos menores para poder pasarle porcentajes con coma

---
+++ trunk/test/src/bomberosTest.java
@@ -0,0 +1,45 @@
+package src;
+
+import Bomberos.EstacionDeBomberos;
+import CentralesElectricas.CentralEolica;
+import Edificios.EdificioComercial;
+import Edificios.EdificioIndustrial;
+import Edificios.EdificioResidencial;
+import Edificios.Posicion;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import Edificios.Reconstruible;
+import Jugador.Presupuesto;
+import Jugador.PresupuestoBajo;
+
+
+public class bomberosTest {
+	@Test
+	public void reconstruccionDeResidencialyEolica(){
+		
+		Posicion unaPosicion=new Posicion(2,1);
+		EdificioResidencial unEdificioResidencial=new EdificioResidencial(unaPosicion);
+		unEdificioResidencial.destruirUnPoco();
+		
+		Posicion otraPosicion=new Posicion(5,1);
+		CentralEolica unaCentral=new CentralEolica(otraPosicion);
+		unaCentral.destruirUnPoco();
+
+		Presupuesto presupuesto = new PresupuestoBajo();
+		EstacionDeBomberos unaEstacionDeBomberos=new EstacionDeBomberos(presupuesto);
+		float porcentajeEsperado=81;
+		Assert.assertEquals(unEdificioResidencial.porcentajeDeConstruccion(),porcentajeEsperado);//81=sin reparar, 91=reparado
+		Assert.assertEquals(unaCentral.porcentajeDeConstruccion(),porcentajeEsperado);
+		
+		unaEstacionDeBomberos.agregarReconstruible(unEdificioResidencial);
+		unaEstacionDeBomberos.agregarReconstruible(unaCentral);
+		unaEstacionDeBomberos.realizarReparaciones();
+		
+		porcentajeEsperado=91;
+		Assert.assertEquals(Math.round(unEdificioResidencial.porcentajeDeConstruccion()),Math.round(porcentajeEsperado));
+		porcentajeEsperado=95;
+		Assert.assertEquals(unaCentral.porcentajeDeConstruccion(),porcentajeEsperado);
+	}
+}
