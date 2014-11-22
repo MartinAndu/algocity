afb8cd1893b49936dfa797561a686520261b52c3
elo94@outlook.com <elo94@outlook.com@ca99f4f7-bc75-fa1d-a80f-d705e4e3179e>
2014-11-22 05:49:11 +0000

    Agregado el generador de superficie deterministico que al generar superficie alterna entre agua o tierra.

--- trunk/src/SuperficiesGeneradores/GeneradorSuperficie.java
+++ trunk/src/SuperficiesGeneradores/GeneradorSuperficie.java
@@ -3,6 +3,7 @@ package SuperficiesGeneradores;
 import Superficies.Superficie;
 
 public abstract class GeneradorSuperficie {
+	public int numeroAlternador = 0;
 
 	public abstract Superficie generarSuperficie();
 }
