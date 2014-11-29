93892527a3e2d96aef291444596b1e7ae0113cb3
joaquinbf@live.com <joaquinbf@live.com@ca99f4f7-bc75-fa1d-a80f-d705e4e3179e>
2014-11-29 14:28:20 +0000

    Cambios en estacion de bomberos y generadoresDeCatastrofes

--- trunk/src/CatastrofeGeneradores/GeneradorCatastrofe.java
+++ trunk/src/CatastrofeGeneradores/GeneradorCatastrofe.java
@@ -3,7 +3,6 @@ package CatastrofeGeneradores;
 import Catastrofe.Catastrofe;
 
 public abstract class GeneradorCatastrofe {
-	static public int numeroAlternador = 0;
 	
 	public abstract Catastrofe generarCatastrofe();
 }
