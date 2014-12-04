a82c18a2e740e86aa3460c738a37fdb40616adcc
joaquinbf@live.com <joaquinbf@live.com@ca99f4f7-bc75-fa1d-a80f-d705e4e3179e>
2014-12-04 23:15:23 +0000

    Turno. en estado de prueba

--- trunk/src/Jugador/Turno.java
+++ trunk/src/Jugador/Turno.java
@@ -8,20 +8,35 @@ public class Turno implements Runnable{
 	private Ambiente ambiente;
 	private Thread hilo;
 	private boolean juegoContinua;
-
+	private int duracion;
+	
 	public Turno(Jugador jugador, Ambiente ambiente) {
 		this.jugador = jugador;
 		this.ambiente = ambiente;
 		this.hilo = new Thread(this);
 		this.juegoContinua = true;
+		this.duracion = 3000;
+		
 	}
 		
 		
 	@Override
 	public void run() {
 		while (this.juegoContinua) {
-			
+			try {
+				this.hilo.wait(duracion);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("Soy el hilo ");
+			//this.jugador.pasoUnTurno();
+			//this.ambiente.pasoUnTurno();
 		}
+	}
+	
+	public void terminar() {
+		this.juegoContinua = false;
 	}
 
 }
