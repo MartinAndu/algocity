308e4ae8df584df66ed3d2d87848c916d4db1209
andujar06@gmail.com <andujar06@gmail.com@ca99f4f7-bc75-fa1d-a80f-d705e4e3179e>
2014-12-04 03:42:11 +0000

    Sigo con la Interfaz Gráfica

---
+++ src/aplicacion/Main.java
@@ -0,0 +1,8 @@
+package aplicacion;
+
+public class Main {
+	public static void main(String args[]){
+		VentanaPrincipal a = new VentanaPrincipal();
+		a.mostrarVentana();
+	}
+}
---
+++ src/aplicacion/PanelDeConstruccion.java
@@ -0,0 +1,32 @@
+package aplicacion;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+
+public class PanelDeConstruccion extends JPanel implements ActionListener{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	PanelDeConstruccion(){
+		this.setLayout(new GridLayout(6,1));
+		this.add(new JButton("Residencia"));
+		this.add(new JButton("Industria"));
+		this.add(new JButton("Comercio"));
+		this.add(new JButton("Pozo De Agua"));
+		this.add(new JButton("Tuberia"));
+		this.add(new JButton("Central"));
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
