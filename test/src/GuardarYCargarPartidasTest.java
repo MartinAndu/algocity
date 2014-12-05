98e461881c9c6554e41186d1954de576781fc553
elo94@outlook.com <elo94@outlook.com@ca99f4f7-bc75-fa1d-a80f-d705e4e3179e>
2014-12-05 17:06:16 +0000

    Unas mejoras en la hidratacion(carga) de la hectarea.

--- test/src/GuardarYCargarPartidasTest.java
+++ test/src/GuardarYCargarPartidasTest.java
@@ -29,13 +29,12 @@ public class GuardarYCargarPartidasTest {
 		Jugador unJugador = unJuego.crearJugador("Jorge");
 		
 		int presupuestoInicial = unJugador.obtenerPresupuesto();
+		
+		Posicion unaPosicion = new Posicion(1,1);
+		
+		unJugador.crearTuberiaDeAgua(unaPosicion);
+		unJugador.crearResidencia(unaPosicion);
 
-		unJugador.crearPozoDeAgua(new Posicion(4,4));
-		
-		unJugador.crearResidencia(new Posicion(1,1));
-		
-		unJugador.crearResidencia(new Posicion(2,2));
-		
 		Assert.assertTrue(unJugador.obtenerPresupuesto() < presupuestoInicial);
 
 		Assert.assertTrue(unJuego.hayJugadores());
@@ -53,7 +52,9 @@ public class GuardarYCargarPartidasTest {
 		Assert.assertTrue(otroAlgoCity.hayJugadores());
 
 		Jugador jorgeResucitado = otroAlgoCity.obtenerPrimerJugadorRegistrado();
+		
 		Assert.assertNotNull(jorgeResucitado);
+		
 		boolean excepcionAtrapada = false;
 		try{
 			jorgeResucitado.crearResidencia(new Posicion(1,1));
@@ -66,5 +67,50 @@ public class GuardarYCargarPartidasTest {
 		
 		Assert.assertTrue(jorgeResucitado.obtenerPresupuesto() < presupuestoInicial);
 
+	}
+	
+	@Test
+	public void probandoGuardarCargarJugadorConDiferentesConstruccionesEnElPlano() throws Exception{
+		AlgoCity unJuego = new AlgoCity();
+		
+		Jugador unJugador = unJuego.crearJugador("Jorge");
+		
+		int presupuestoInicial = unJugador.obtenerPresupuesto();
+		
+		Posicion unaPosicion = new Posicion(1,1);
+		
+		unJugador.crearRutaPavimentada(unaPosicion);
+		unJugador.crearCentralEolica(new Posicion(1,2));
+		unJugador.crearPozoDeAgua(new Posicion(1,4));
+		unJugador.crearTuberiaDeAgua(new Posicion(1,4));
+		
+		Assert.assertTrue(unJuego.hayJugadores());
+		
+		unJugador.guardarPartida(); 
+		
+
+		AlgoCity otroAlgoCity = new AlgoCity();
+		
+		Assert.assertTrue(!otroAlgoCity.hayJugadores());
+		
+		otroAlgoCity.cargarPartidaJugador("Jorge.xml");
+		
+		Assert.assertTrue(otroAlgoCity.hayJugadores());
+
+		Jugador jorgeResucitado = otroAlgoCity.obtenerPrimerJugadorRegistrado();
+		
+		Assert.assertNotNull(jorgeResucitado);
+		
+		boolean excepcionAtrapada = false;
+		try{
+			jorgeResucitado.crearResidencia(new Posicion(1,1));
+		}
+		catch (ExcepcionHectareaYaContieneUnaConstruccion excepcion){
+			excepcionAtrapada = true;
+		}
+        
+        Assert.assertTrue(excepcionAtrapada);
+		
+		Assert.assertTrue(jorgeResucitado.obtenerPresupuesto() < presupuestoInicial);
 	}
 }
